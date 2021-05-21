3166343b02a4594c0ec831f89d6d3e8dfe887625
hp <nivedithahrram2017@gmail.com>
2021-05-21 20:52:06 +0530

    FRONT END

---
+++ app/src/main/java/com/example/tourit/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.tourit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
